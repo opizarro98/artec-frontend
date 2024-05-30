f960aa07e95396b0a2a0b62a5cde642bbcd6679e
opizarro98 <131551382+opizarro98@users.noreply.github.com>
2024-05-29 19:49:10 -0500

    OP - add Fotter page and structure  main page 50%

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
+import { FooterComponent } from './pages/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MainComponent],
+  imports: [CommonModule, RouterOutlet, MainComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,26 +1,87 @@
+import { CommonModule } from '@angular/common';
 import { Component,HostListener } from '@angular/core';
-import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
+
+interface Event {
+  image: string;
+  title: string;
+  date: string;
+  location: string;
+  description: string;
+}
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CarouselModule],
+  imports: [CommonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
     title = 'Bienvenido a Artec';
 
-  carouselOptions: OwlOptions = {
-    loop: true,
-    autoplay: true,
-    autoplayTimeout: 3000,
-    autoplayHoverPause: true,
-    nav: true,
-    navText: ['<', '>'],
-    items: 1,
-    dots: true
-  };
+      events: Event[] = [
+    { 
+      image: 'assets/event1.jpg', 
+      title: 'Evento 1', 
+      date: 'Mayo 9, 2024', 
+      location: 'Quito - MicroTeatro UIO', 
+      description: 'Descripción del evento 1.' 
+    },
+    { 
+      image: 'assets/event2.jpg', 
+      title: 'Evento 2', 
+      date: 'Mayo 10, 2024', 
+      location: 'Guayaquil - Teatro Centro de Arte', 
+      description: 'Descripción del evento 2.' 
+    },
+    { 
+      image: 'assets/event1.jpg', 
+      title: 'Evento 1', 
+      date: 'Mayo 9, 2024', 
+      location: 'Quito - MicroTeatro UIO', 
+      description: 'Descripción del evento 1.' 
+    },
+    { 
+      image: 'assets/event2.jpg', 
+      title: 'Evento 2', 
+      date: 'Mayo 10, 2024', 
+      location: 'Guayaquil - Teatro Centro de Arte', 
+      description: 'Descripción del evento 2.' 
+    },
+    { 
+      image: 'assets/event1.jpg', 
+      title: 'Evento 1', 
+      date: 'Mayo 9, 2024', 
+      location: 'Quito - MicroTeatro UIO', 
+      description: 'Descripción del evento 1.' 
+    },
+    { 
+      image: 'assets/event2.jpg', 
+      title: 'Evento 2', 
+      date: 'Mayo 10, 2024', 
+      location: 'Guayaquil - Teatro Centro de Arte', 
+      description: 'Descripción del evento 2.' 
+    },
+    { 
+      image: 'assets/event1.jpg', 
+      title: 'Evento 1', 
+      date: 'Mayo 9, 2024', 
+      location: 'Quito - MicroTeatro UIO', 
+      description: 'Descripción del evento 1.' 
+    },
+    { 
+      image: 'assets/event2.jpg', 
+      title: 'Evento 2', 
+      date: 'Mayo 10, 2024', 
+      location: 'Guayaquil - Teatro Centro de Arte', 
+      description: 'Descripción del evento 2.' 
+    },
+    
+  ];
+
+  currentPage = 1;
+  eventsPerPage = 12;
+
   @HostListener('window:scroll', [])
   onWindowScroll() {
     const header = document.getElementById('header');
@@ -36,7 +97,30 @@ export class MainComponent {
   onFilterChange(event: any) {
     const selectedCity = event.target.value;
     console.log('Selected city:', selectedCity);
-    // Aquí puedes agregar lógica para filtrar eventos según la ciudad seleccionada
   }
 
+   get paginatedEvents(): Event[] {
+    const startIndex = (this.currentPage - 1) * this.eventsPerPage;
+    return this.events.slice(startIndex, startIndex + this.eventsPerPage);
+  }
+
+  get totalPages(): number {
+    return Math.ceil(this.events.length / this.eventsPerPage);
+  }
+
+  setPage(page: number) {
+    this.currentPage = page;
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
 }
