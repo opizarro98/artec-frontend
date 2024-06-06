c982219126bf67db74a9bedef80017b3e5d5cf2e
kity12cris <cristina12_92@hotmail.com>
2024-06-06 16:44:00 -0500

    OP - Add login service file to send backend request to login person

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { LoginService } from '../../../external_service/service/login.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink,RouterOutlet],
+  imports: [RouterLink,RouterOutlet,ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private loginService: LoginService) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const { username, password } = this.loginForm.value;
+      this.loginService.login(username, password).subscribe(
+        response => {
+          console.log('Login exitoso:', response);
+          // Manejar el login exitoso, como redirigir al usuario
+        },
+        error => {
+          console.error('Login fallido:', error);
+          alert('Error en el login. Por favor, intenta de nuevo.');
+        }
+      );
+    }
+  }
 }
---
+++ src/external_service/service/login.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  private BASE_URL = 'http://localhost:8090/person';
+
+  constructor(private http: HttpClient) {}
+
+  login(username: string, password: string): Observable<any> {
+    console.log("Se llama al servicio: "+ this.BASE_URL+ "/login/"+ username +"/"+password);
+    return this.http.get<any>(`${this.BASE_URL}/login/${username}/${password}`)
+      .pipe(
+        catchError(error => {
+          console.error('Error en loginService:', error);
+          throw error;
+        })
+      );
+  }
+}
